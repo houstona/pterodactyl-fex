7496b055896fd37a56714d7bdfddf4d1c038014d
Arron Houston <houstona@users.noreply.github.com>
2026-04-14 10:25:29 -0400

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cd /home/container
+
+# Internal Pterodactyl variable replacement
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+
+# 1. Export critical variables
+export FEX_TSO=1
+export FEX_PASS_THROUGH_USER=1
+export USER=container
+export HOME=/home/container
+
+# 2. Check if we are already in FEXBash; if not, re-run this script inside it
+if [ "$(uname -m)" != "x86_64" ]; then
+    echo "Transitioning to x86_64 environment via FEXBash..."
+    exec FEXBash -c "$0 $@"
+fi
+
+# --- Everything below this line runs as x86_64 ---
+
+# 3. Fix Open Files Limit
+ulimit -n 65535
+
+# 5. Run the startup command directly
+# Since we are already in FEXBash, we don't need to call it again in the startup
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+eval ${MODIFIED_STARTUP}
